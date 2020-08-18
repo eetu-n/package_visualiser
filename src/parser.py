f646fb6ae31c1d09d4ca6159b0f022a92ad98a67
rargh <eetu.t.narhi@gmail.com>
2020-08-18 23:02:09 +0300

    Initial parser completed

--- src/parser.py
+++ src/parser.py
@@ -1,4 +1,5 @@
 import re
+import json
 
 class pkg_status:
     pass
@@ -14,33 +15,30 @@ def read_pkg_status(file_name: str):
 
     for package in file:
         package = package.split("\n")
-        new_package = []
+        split_package = []
 
         # 'line' starts with space if part of multiline value
         for line in package:
             if len(line) == 0:
                 pass
+
             elif line[0] == ' ':
-                new_package[-1] = new_package[-1] + '\n' + line[1:]
+                split_package[-1] = split_package[-1] + '\n' + line[1:]
+
             else:
-                new_package.append(line)
+                split_package.append(line)
 
-        package = new_package
-        new_package = {}
+        package_dict = {}
 
-        for line in package:
-            print(parse_key_val_pair(line))
-            new_package.update(parse_key_val_pair(line))
+        for line in split_package:
+            package_dict.update(parse_key_val_pair(line))
 
-        package_list.append(new_package)
+        package_list.append(package_dict)
 
-    
-    
-    print(package_list[0])
-
+    return package_list
 
 def parse_key_val_pair(line: str):
-    pair    = line.split(": ", 1)
+    pair    = re.split(":[\n ]", line, 1)
     if len(pair) <= 1:
         return line
     key     = pair[0]
@@ -62,8 +60,8 @@ def parse_key_val_pair(line: str):
         pass
 
     elif key == "Maintainer":
-        value = value[:-1]
-        value = value.split(" <", 1)
+        # Transform into (name, email) tuple
+        value = tuple(value[:-1].split(" <", 1))
 
     elif key == "Architecture":
         pass
@@ -90,14 +88,22 @@ def parse_key_val_pair(line: str):
         value = value.split(", ")
 
     elif key == "Conffiles":
+        # Split lines to list with each line being tuple object
         value = value.replace("\n ", "\n")
+        value = value.split("\n")
+        split_value = []
+        for line in value:
+            split_value.append(tuple(line.split(" ")))
+        
+        value = split_value
 
     elif key == "Description":
+        # Remove space from beginnings of lines
         value = value.replace("\n ", "\n")
 
     elif key == "Original-Maintainer":
-        value = value[:-1]
-        value = value.split(" <", 1)
+        # Transform into (name, email) tuple
+        value = tuple(value[:-1].split(" <", 1))
 
     elif key == "Homepage":
         pass
